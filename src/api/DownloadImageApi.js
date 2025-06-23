b16fcaa3da765f22ea491df406ade843af043c84
beti <bitasaeedii80@Gmail.com>
2025-06-23 09:14:46 +0330

    image url

--- src/api/DownloadImageApi.js
+++ src/api/DownloadImageApi.js
@@ -1,4 +1,4 @@
 export const DownloadImageApi =  (imageAddres) => {
-  const url = `https://file.shabinja.com/api/v1/file/Download?path=${imageAddres}`;
+  const url = `http://fileshabinja.zookaar.com/api/v1/file/Download?path=${imageAddres}`;
   return url;
 };
