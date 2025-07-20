8544f739da1d75ee9c8bef70d92c86788471a5a1
beti <bitasaeedii80@Gmail.com>
2025-07-20 09:31:02 +0330

    fast search for mobile

--- src/pages/Home/Components/SubSliderHeader.js
+++ src/pages/Home/Components/SubSliderHeader.js
@@ -1,25 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Box, Grid, Button, Typography } from "@mui/material";
-import VillaIcon from "@mui/icons-material/Villa";
-import HomeIcon from "@mui/icons-material/Home";
-import NaturePeopleIcon from "@mui/icons-material/NaturePeople";
-import PoolIcon from "@mui/icons-material/Pool";
-import BeachAccessIcon from "@mui/icons-material/BeachAccess";
-import AgricultureIcon from "@mui/icons-material/Agriculture";
-import StarIcon from "@mui/icons-material/Star";
+import { Box, Grid, useMediaQuery } from "@mui/material";
 import FastSearchCard from "../../../components/Cards/FastSearchCard";
 import ItemsFastSearch from "../../../myDatas/ItemsFastSearch";
+import { useTheme } from "@emotion/react";
+import FastSearchCardMobile from "../../../components/Cards/FastSearchCardMobile";
 
-const SubSliderHeader = ({listCategories = []}) => {
- 
+const SubSliderHeader = ({ listCategories = [] }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
+  console.log("listCategories", listCategories);
 
   return (
-    <Grid container justifyContent="space-around" alignItems="center">
-      {listCategories.map((item, index) => (
-        <FastSearchCard key={index} myData={item} />
-      ))}
-    </Grid>
+    <>
+      {isMobile ? (
+        <Grid
+          sx={{
+            padding: "0 12px",  
+          }}
+          container
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          {listCategories.map((item, index) => (
+            <FastSearchCardMobile key={index} myData={item} />
+          ))}
+        </Grid>
+      ) : (
+        <>
+          <Grid container justifyContent="space-around" alignItems="center">
+            {listCategories.map((item, index) => (
+              <FastSearchCard key={index} myData={item} />
+            ))}
+          </Grid>
+        </>
+      )}
+    </>
   );
 };
 
