c7cf529b6bc5bebb46e5804b7e5b03d676fec3c4
beti <bitasaeedii80@Gmail.com>
2025-08-12 11:54:16 +0330

    Banner

--- src/pages/Home/Components/Begust/Begust.js
+++ src/pages/Home/Components/Begust/Begust.js
@@ -1,16 +1,53 @@
 import { Button, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import API_URL from "../../../../config/apiConfig";
+import { DownloadImageApi } from "../../../../api/DownloadImageApi";
+const baseUrl = API_URL;
 const Begust = () => {
+
+  const [bannerInfo, setBannerInfo]=useState();
+  const [bgImage,setBgImage]=useState();
+
+  const getBannerInfos = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await axios.get(
+        `${baseUrl}/BanerData`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("banner",response?.data);
+      setBannerInfo(response?.data?.data);
+      let bg = DownloadImageApi(response?.data?.data?.image?.url)
+      setBgImage(bg)
+    } catch (error) {
+      console.log(error?.response);
+      return error?.response;
+    }
+  };
+
+  useEffect(()=>{
+    getBannerInfos();
+  },[])
+
+  
   return (
     <Box
       sx={{
-        py: { xs: 3, md: 5 },
+        py: { xs: 3, md: 10},
         px: { xs: 2, md: 4 },
-        bgcolor: "#0d47a1",
-        background: "linear-gradient(135deg, #0d47a1 0%, #1976d2 100%)",
+        backgroundImage: bannerInfo?.image?.url
+          ? `url(${bgImage})`
+          : "linear-gradient(135deg, #0d47a1 0%, #1976d2 100%)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
         color: "white",
         mb: 0,
         textAlign: "center",
@@ -29,7 +66,7 @@ const Begust = () => {
               fontSize: { xs: "1.8rem", md: "2.5rem" },
             }}
           >
-            میزبانی و کسب درآمد
+            {bannerInfo?.title ||""}
           </Typography>
           <Typography
             variant="h6"
@@ -42,18 +79,17 @@ const Begust = () => {
               opacity: 0.9, // Softer contrast for readability
             }}
           >
-            به جمع میزبانان بپیوندید و اقامتگاه خود را ثبت کنید و شروع کنید به
-            درآمد زایی
+           {bannerInfo?.text || ""}
           </Typography>
         </Grid>
 
         <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
           <Button
             component={Link}
-            to="/new-stay/start"
+            to={bannerInfo?.btnLink}
             variant="contained"
             sx={{
-              background: "linear-gradient(45deg, #ff9800, #ff5722)",
+              background: bannerInfo?.btnColorCode,
               px: 4,
               py: 1.5,
               borderRadius: 3,
@@ -65,7 +101,7 @@ const Begust = () => {
               },
             }}
           >
-            بیشتر بدانید
+            {bannerInfo?.btnTitle || ""}
           </Button>
         </Grid>
       </Grid>
