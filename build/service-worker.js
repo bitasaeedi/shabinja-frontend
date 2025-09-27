28877293f756a2ca6354028c7f3b498a6bbef1a6
beti <bitasaeedii80@Gmail.com>
2025-09-27 11:27:26 +0330

    build

---
+++ build/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "shabinja-cache-v1";
+const urlsToCache = [
+  "/",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.ico",
+  "./android-chrome-192x192.png",
+  "./android-chrome-512x512.png",
+  "./offline.html"
+  // میتونی مسیر اسکرین‌شات‌ها و فایل‌های استاتیک دیگه هم اضافه کنی
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).catch(() => caches.match("/offline.html"));
+    })
+  );
+});
